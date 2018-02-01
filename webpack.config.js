e48800fe2c512c615db447097523e636766757a2
hux <798947970@qq.com>
2018-02-02 00:17:01 +0800

    add less compress

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,17 @@ module.exports = {
                         loader : 'html-withimg-loader'
                     }
                 ]
+            },
+            {
+                test : /\.less$/,
+                use : extractTextPlugin.extract({
+                    use : [{
+                        loader:'css-loader'
+                    },{
+                        loader : 'less-loader'
+                    }],
+                    fallback : 'style-loader'
+                })
             }
         ]  
     },
